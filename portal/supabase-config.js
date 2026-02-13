784caf320cfb39576706cc1bcf4d041ee42b3446
bccarneiro-cloud <bruno.ccarneiro@gmail.com>
2026-02-13 11:31:40 +0100

    Update supabase-config.js

--- portal/supabase-config.js
+++ portal/supabase-config.js
@@ -1,4 +1,4 @@
 // Troca pelos teus valores do Supabase:
 // Settings → API → Project URL e anon public key
-https://xwdzqggmlzghzizzdouv.supabase.co
-eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inh3ZHpxZ2dtbHpnaHppenpkb3V2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzA5NzY0OTgsImV4cCI6MjA4NjU1MjQ5OH0.k8KF0ZWQZXQFZSU-I3fxIKHpw76dsI_l7P6do1h4nvY
+window.SUPABASE_URL = "https://xwdzqggmlzghzizzdouv.supabase.co";
+window.SUPABASE_ANON_KEY = "sb_publishable_eYy4VC4tEbvlHB9PowX-cQ_ZWvTEF79";
